2d167d4f5fa49f3b467e58e5e33ec9c132d8baec
Tomer Baum <tbaum@proofpoint.com>
2021-08-29 00:45:39 +0300

    add other info update default/greet.py file

--- packages/default/greet.py
+++ packages/default/greet.py
@@ -1,6 +1,6 @@
 def main(args):
-    name = args.get('name', "World2")
-    name = name if name else "World"
+    name = args.get('name', ".")
+    name = name if name else "."
     message = "python file test Upload file for Ronen and Tomer" + name + "!";
     print(message)
     return {
