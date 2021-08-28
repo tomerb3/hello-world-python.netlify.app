726e6fd1ec8c83a34bb6524dd41b543187089703
Tomer Baum <tbaum@proofpoint.com>
2021-08-29 00:45:12 +0300

    add script code to git update default/greet.py file

--- packages/default/greet.py
+++ packages/default/greet.py
@@ -1,7 +1,7 @@
 def main(args):
     name = args.get('name', "World2")
     name = name if name else "World"
-    message = "Upload file for Ronen and Tomer" + name + "!";
+    message = "python file test Upload file for Ronen and Tomer" + name + "!";
     print(message)
     return {
         "body": {"message": message}
